4f2b6981032eb149ac1de00ada9cded2aefd919d
viet tu <quyen.luminous@gmail.com>
2023-08-15 19:27:37 +0700

    (!) add some ipfs data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useEffect, useState} from 'react';
+import {Button, Container, Row, Col} from "react-bootstrap";
+import uri from "./uri.json";
+import { createHelia } from 'helia';
 function App() {
+    const [data, setData] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            // create a Helia node
+            const helia = await createHelia();
+
+            // get data from IPFS
+            const cid = 'Qmf6jZezmLcHrUdvrGkNmas9XFWBHQnHu9ABqRD1gWB4yF';
+            for await (const file of helia.get(cid)) {
+                if (!file.content) continue;
+                console.log(file.content.toString());
+            }
+        };
+        fetchData();
+    }, []);
+
+    return <div>{data}</div>;
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+          <Row>
+              <Col>
+                  <Button>Connect wallet</Button>
+              </Col>
+          </Row>
+          <Row>
+              <h1>Lazy mint NFT</h1>
+          </Row>
+          {uri.map(e => (<Row>
+              <h3>{e}</h3>
+          </Row>))}
+      </Container>
     </div>
   );
 }
