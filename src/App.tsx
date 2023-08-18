545422b76a6051a83f4dd466e84944e06e34e744
viet tu <quyen.luminous@gmail.com>
2023-08-18 19:18:01 +0700

    (+) NFT display

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,66 @@
 import React, {useEffect, useState} from 'react';
-import {Button, Container, Row, Col} from "react-bootstrap";
-import uri from "./uri.json";
-import { createHelia } from 'helia';
-function App() {
-    const [data, setData] = useState<string | null>(null);
+import {Button, Container, Row, Col, Card, Tabs, Tab} from "react-bootstrap";
+import uris from "./uri.json";
 
+function App() {
+    const [ipfsData, setIpfsData] = useState<any[]>([]);
+    const ipfsUrl = 'https://ipfs.io/ipfs/';
+    const processIpfsUrl = (uri: string) => {
+        return ipfsUrl + uri.substring(7, uri.length);
+    }
     useEffect(() => {
         const fetchData = async () => {
-            // create a Helia node
-            const helia = await createHelia();
-
-            // get data from IPFS
-            const cid = 'Qmf6jZezmLcHrUdvrGkNmas9XFWBHQnHu9ABqRD1gWB4yF';
-            for await (const file of helia.get(cid)) {
-                if (!file.content) continue;
-                console.log(file.content.toString());
+            const returnData: any[] = [];
+            for (const uri of uris) {
+                const res = await fetch(processIpfsUrl(uri));
+                const json = await res.json();
+                returnData.push(json);
             }
+            setIpfsData(returnData);
         };
         fetchData();
     }, []);
 
-    return <div>{data}</div>;
-
-
-  return (
-    <div className="App">
-      <Container>
-          <Row>
-              <Col>
-                  <Button>Connect wallet</Button>
-              </Col>
-          </Row>
-          <Row>
-              <h1>Lazy mint NFT</h1>
-          </Row>
-          {uri.map(e => (<Row>
-              <h3>{e}</h3>
-          </Row>))}
-      </Container>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Container>
+                <Row>
+                    <Col>
+                        <Button>Connect wallet</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <h1>Lazy mint NFT</h1>
+                </Row>
+                <Row>
+                    {ipfsData.map(e => (
+                        <Col>
+                            <Card style={{width: '18rem'}}>
+                                <Card.Img variant="top" src={processIpfsUrl(e.image)}/>
+                                <Card.Body>
+                                    <Card.Title>{e.name}</Card.Title>
+                                    <Card.Text>{e.description}</Card.Text>
+                                    <Tabs>
+                                        {e.attributes.map((attr: any) => (
+                                            <Tab eventKey={attr.trait_type} title={attr.trait_type}>
+                                                {attr.value}
+                                            </Tab>
+                                        ))}
+                                    </Tabs>
+                                    <Button variant="primary">Mint this NFT</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+                <Row>
+                    <h1>Your wallet:</h1>
+                    <h2>tBNB balance: </h2>
+                    <h2>NFT balance: </h2>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
